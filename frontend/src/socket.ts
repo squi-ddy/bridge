462f0b4cec7e83364e2483584616a8e53bbb101f
a squid <34890855+squi-ddy@users.noreply.github.com>
2024-08-28 08:20:26 +0800

    Update socket.ts

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -9,5 +9,5 @@ export function createSocket(): Socket<
     ServerToClientEvents,
     ClientToServerEvents
 > {
-    return io(settings.SOCKET_URL, { transports: ["websocket"] })
+    return io(settings.SOCKET_URL, { transports: ["websocket"], path: "socket.io" })
 }
