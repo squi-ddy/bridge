f2ce8a8d3adbfa1eea8399a8a3aad98f6ee093d1
a squid <34890855+squi-ddy@users.noreply.github.com>
2024-08-28 08:30:41 +0800

    fix socketio

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -1,5 +1,6 @@
 const settings = {
     SOCKET_URL: import.meta.env.VITE_SOCKET_URL || "http://localhost:3001",
+    SOCKET_SERVER_PATH: (import.meta.env.VITE_SOCKET_SERVER_PATH || "/") + "socket.io/"
 }
 
 export { settings }
--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -9,5 +9,5 @@ export function createSocket(): Socket<
     ServerToClientEvents,
     ClientToServerEvents
 > {
-    return io(settings.SOCKET_URL, { transports: ["websocket"], path: "socket.io" })
+    return io(settings.SOCKET_URL, { transports: ["websocket"], path: settings.SOCKET_SERVER_PATH })
 }
