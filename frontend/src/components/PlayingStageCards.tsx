b33cb8d6dab6d980eebcc9bfd241d8eee57d9fd0
hemisemidemipresent <hartmannsyg@gmail.com>
2024-10-02 16:18:21 +0800

    bidding table + bidding history + balatro

--- frontend/src/components/PlayingStageCards.tsx
+++ frontend/src/components/PlayingStageCards.tsx
@@ -1,15 +1,20 @@
+import { useContext } from 'react';
+import { GlobalContext } from "@/base/BasePage"
 import { cardToCardURL } from "@/util/cards"
 import { Card } from "@backend/types/Card"
 
 function PlayingStageCards(props: { cards: Card[]; playerNames: string[] }) {
+
+    const { globalContext } = useContext(GlobalContext)
+
     return (
         <div className="flex w-full gap-2 mb-5 justify-center">
             {props.cards.map((card, idx) => (
                 <div
-                    key={cardToCardURL(card)}
+                    key={cardToCardURL(card, globalContext.balatro)}
                     className="flex flex-col gap-2 items-center"
                 >
-                    <img src={cardToCardURL(card)} className={`w-40`} />
+                    <img src={cardToCardURL(card, globalContext.balatro)} className={`w-40 rounded-sm`} style={{imageRendering: "pixelated"}}/>
                     <p className="text-2xl">From {props.playerNames[idx]}</p>
                 </div>
             ))}
