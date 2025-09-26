9bd2e1e2ad17a5a854cc573a025663762ea27e30
squiddy <squid2015guy@gmail.com>
2025-09-26 12:57:49 +0800

    update tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import typescript from "@rollup/plugin-typescript"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,6 @@ export default defineConfig({
                 "../backend/src/types/*.ts",
             ],
         }),
+        tailwindcss(),
     ],
 })
